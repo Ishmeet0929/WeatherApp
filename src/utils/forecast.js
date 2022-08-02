8df4c9a016bf33f69fc2aafc37b5159e31c330d8
Ishmeet0929 <78226805+Ishmeet0929@users.noreply.github.com>
2022-08-02 16:05:13 +0530

    Add files via upload

---
+++ src/utils/forecast.js
@@ -0,0 +1,25 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=dd320da50dec702b32ae5a75278d413a&query=${latitude},${longitude}&units=m`;
+
+    request({ url, json: true }, (error, { body } = {}) => {
+        if (error) {
+            callback('Unabale to connect to weather service!', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(undefined, {
+                localtime: body.location.localtime,
+                icon: body.current.weather_icons,
+                description: body.current.weather_descriptions[0],
+                windSpeed: `${body.current.wind_speed} Kilometers/Hour`,
+                pressure: `${body.current.pressure} Millibar`,
+                humidity: `${body.current.humidity} g.m-3`,
+                forecast: `It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+            });
+        }
+    });
+};
+
+module.exports = forecast;
